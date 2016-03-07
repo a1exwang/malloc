2127b076277332b5dc1396276b6006b567d59076
Alex Wang <ice_b0und@hotmail.com>
2016-03-07 09:37:23 +0800

    finish best alloc without free

--- malloc.c
+++ malloc.c
@@ -78,8 +78,6 @@ void *mymalloc(int size) {
 	} while (current != &free_head);
 	return 0;
 }
-
-
 void myfree(void *mem) {
 	list_node_t *current = free_head.next;
 	while (current != &free_head) {
@@ -94,6 +92,68 @@ void myfree(void *mem) {
 
 				// 释放next, 并且维护链表
 				
+				current->next = next->next;
+				next->next = current;
+				list_free(next);
+			}
+		}
+		current = current->next;
+	}
+}
+
+void *my_best_alloc(int size) {
+	if (total_free_size < size) {
+		sbrk(size);
+	}
+	list_node_t *current = free_head.next;
+	int min = BRK_SIZE;
+	list_node_t *min_node;
+	do {
+		// 找到最小且足够的块
+		if (current->free && current->size >= size) {
+			if (current->size < min) {
+				min_node = current;
+				min = current->size;
+			}
+		}
+		current = current->next;
+	} while (current != &free_head);
+
+	if (min == BRK_SIZE) {
+		return 0;
+	}
+
+	current = min_node;
+	void *ret = current->start;
+	current->free = 0;
+
+	// 如果空闲块比需要的大则把剩余的部分新建成为一个块
+	if (current->size != size) {
+		list_node_t *new_node = list_alloc();
+		new_node->free = 1;
+		new_node->size = current->size - size;
+		new_node->start = (char*)current->start + size;
+		list_add(current, new_node);
+
+		current->size = size;
+	}
+	total_free_size -= size;
+	return ret;
+}
+void my_best_free(void *mem) {
+	list_node_t *current = free_head.next;
+	while (current != &free_head) {
+		if (current->start == mem) {
+			current->free = 1;
+			total_free_size += current->size;
+			if (current->next->free &&
+				(char*)current->start + current->size == (char*)current->next->start) {
+				list_node_t *next = current->next;
+				// 合并current和next
+				current->size += next->size;
+
+				// 释放next, 并且维护链表
+
 				current->next = next->next;
 				next->next = current;
 				list_free(next);
